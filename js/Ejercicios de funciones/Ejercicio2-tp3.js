ae0a4e9f005d6eb663e994c9d6d1fddd076c148a
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-07-22 18:00:09 -0300

    hice ejercicio 2 de funciones

---
+++ js/Ejercicios de funciones/Ejercicio2-tp3.js
@@ -0,0 +1,19 @@
+function info(cadena) {
+
+    let resultado = "La cadena \""+cadena+"\" ";
+  
+    // Comprobar mayúsculas y minúsculas
+    if(cadena == cadena.toUpperCase()) {
+      resultado += " está formada sólo por mayúsculas";
+    }
+    else if(cadena == cadena.toLowerCase()) {
+      resultado += " está formada sólo por minúsculas";
+    }
+    else {
+      resultado += " está formada por mayúsculas y minúsculas";
+    }
+  
+    return resultado;
+  }
+  
+  alert(info(prompt("ingrese una cadena")));
