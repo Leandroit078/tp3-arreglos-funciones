085506120ed3eb6f9e2d073f47fb60a6076bf78d
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-07-22 17:06:48 -0300

    termine el ejercicio 3 viendo la clase

--- js/Ejercicios de arreglos/Ejercicio3-tp3.js
+++ js/Ejercicios de arreglos/Ejercicio3-tp3.js
@@ -18,21 +18,25 @@ for (let i = 0; i < 50; i++) {
     pruebaDado2.push(dado2);
 }
 
-document.write("<br>Resultado del primer dado" + pruebaDado1)
-document.write("<br>Resultado del primer dado" + pruebaDado2)
-document.write("<br>Resultado de la suma" + suma)
+document.write("<br>Resultado del primer dado " + pruebaDado1);
+document.write("<br>Resultado del primer dado " + pruebaDado2);
+document.write("<br>Resultado de la sumas: " + sumas);
 
 document.write(`<table>
 <tbody>
 <tr><td>Resultado</td><td>Frecuencia</td></tr>
 `)
-for (let resultado = 2; ; resultado <= 12; resultado++) {
-    for (let posicion = 0; posicion < suma.length; posicion++) {
-        if (resultado === sumas[posicion]) {
+
+//este se encarga de dibujar las filas
+for(let resultado =2; resultado <=12; resultado++){
+    let contador=0;
+    for(let posicion =0; posicion < sumas.length; posicion++){
+        if(resultado === sumas[posicion]){
             //sumar las posiciones
+            contador++
         }
     }
-    document.write(`<tr><td>${resultado}</td>x</td></tr>`);
+    document.write(`<tr><td>${resultado}</td><td>${contador}</td></tr>`);
 }
 
 document.write(`</tbody></table>`)
