0433de2c1c5e77c4876adf93a16b1ddf0a93e11b
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-07-24 19:47:33 -0300

    hice ejercicio 2 de arreglos

--- js/Ejercicios de arreglos/Ejercicio2-tp3.js
+++ js/Ejercicios de arreglos/Ejercicio2-tp3.js
@@ -1,22 +1,22 @@
+let ciudades2 = [];
 
-let ciudades = []
+do {
+  let Ciudades = prompt("Introduce una ciudad");
+  ciudades2.push (Ciudades)
+} while (confirm("Desea seguir?"));
 
-    do{
-        let ciudad = prompt("Introduce una ciudad");
-    if(ciudades == ""){
-        ciudades.push(parseInt(ciudad));
-}
-    else{
-        ciudades = [ciudades + "-" + ciudad];
-}
-}while(confirm("Desea seguir?"));
-    document.write(ciudades);
-
-    document.write(`<ul>`);
-    for (let indice = 0; indice < ciudades.length; indice++) {
-        document.write(`<li>${ciudades[indice]}</li>`)
-    }
-    document.write(`</ul>`);
-
-
-
+document.write('<br>'+ciudades2);
+//mostrar longuitud de arreglo
+console.log (ciudades2.length)
+//Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+console.log (ciudades2[0])
+console.log (ciudades2[3])
+console.log (ciudades2[ciudades2.length-1])
+//Añade en última posición la ciudad de París.
+ciudades2.push("Paris")
+document.write('<br>'+ciudades2);
+//Escribe por pantalla el elemento que ocupa la segunda posición.
+document.write ('<br>'+ciudades2[1])
+//Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+ciudades2[1] = 'Barcelona';
+document.write('<br>'+ciudades2);
